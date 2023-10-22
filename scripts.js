439e35d3e5ae9ce3b7ed3ddc1541fc16bd7eacdb
PorterAFATW <148170739+PorterAFATW@users.noreply.github.com>
2023-10-22 19:21:10 -0400

    Delete scripts.js

--- scripts.js
+++
@@ -1,9 +0,0 @@
-let Send = document.getElementById("Send");
-const Discord = require("discord.js");
-const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
-
-Send.addEventListener("click", () => {
-    let channel = client.channels.cache.get("1163248541129511016");
-    let Input = document.getElementById("InputMessage");
-    channel.send(Input.value); // Use Input.value to get the input text
-});
